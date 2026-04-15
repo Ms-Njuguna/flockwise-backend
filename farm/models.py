64a148c16aaa1fa914a06ad16eec70422f315048
Patricia <patricianjuguna084@gmail.com>
2026-04-15 21:39:07 +0300

    feat: create record model for storing farm data

--- farm/models.py
+++ farm/models.py
@@ -1,23 +1,7 @@
 from django.db import models
 
-class FlockGroup(models.Model):
-    GROUP_TYPES = [
-        ('chicks', 'Chicks'),
-        ('growers', 'Growers'),
-        ('layers', 'Layers'),
-    ]
-
-    name = models.CharField(max_length=100)
-    group_type = models.CharField(max_length=20, choices=GROUP_TYPES)
-    bird_count = models.IntegerField()
-    age_weeks = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class DailyRecord(models.Model):
-    flock = models.ForeignKey(FlockGroup, on_delete=models.CASCADE)
-    eggs_collected = models.IntegerField(default=0)
-    feed_given_kg = models.FloatField()
-    date = models.DateField(auto_now_add=True)
+class Record(models.Model):
+    eggs = models.IntegerField()
+    income = models.FloatField()
+    profit = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
